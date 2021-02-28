99e2d2b6226a3723f01c972358362b9df3620a1b
Mathieu Tuli <tuli.mathieu@gmail.com>
2021-02-28 12:33:42 -0500

    Add parsing setup for logs and subparser

--- tod/src/tod/__main__.py
+++ tod/src/tod/__main__.py
@@ -3,3 +3,27 @@
 @github: MathieuTuli
 @email: tuli.mathie@gmail.com
 """
+from argparse import ArgumentParser
+from pathlib import Path
+
+import logging
+
+from .utils.logging import init_logger, args as logging_args
+from .train import args as train_args, main as train_main
+
+parser = ArgumentParser(description=__doc__)
+subparser = parser.add_subparsers(dest='command')
+logging_args(parser)
+train_subparser = subparser.add_parser(
+    'train', help='Train commands', parents=[parser])
+train_args(train_subparser)
+
+args = parser.parse_args()
+if args.verbose:
+    args.log_level = logging.DEBUG
+logger = init_logger(Path('logs/tod.log'), level=args.log_level)
+
+if str(args.command) == 'train':
+    train_main(args)
+else:
+    logger.critical(f"Unknown subcommand {args.command}")
--- tod/src/tod/utils/logging.py
+++ tod/src/tod/utils/logging.py
@@ -4,6 +4,8 @@
 @link: https://github.com/OpenNMT/OpenNMT-py/blob/master/onmt/utils/logging.py
 """
 from logging.handlers import RotatingFileHandler
+from argparser import ArgumentParser
+from pathlib import Path
 from enum import Enum
 
 import logging
@@ -20,30 +22,51 @@ class LogLevel(Enum):
     WARNING = logging.WARNING
     ERROR = logging.ERROR
     CRITICAL = logging.CRITICAL
+    NOTSET = logging.NOTSET
 
     def __str__(self):
         return self.name
 
+    @staticmethod
+    def values():
+        return list(map(lambda c: c.value, LogLevel))
 
-def init_logger(log_file: str = None,
-                log_file_level=logging.NOTSET,
-                rotate=False):
+
+def init_logger(log_file: Path,
+                log_level: LogLevel = LogLevel.NOTSET,
+                rotate=False) -> logging.Logger:
+    if log_level not in LogLevel.values():
+        raise ValueError(f"Unknown log level: {log_level}")
     log_format = logging.Formatter("[%(asctime)s %(levelname)s] %(message)s")
     logger = logging.getLogger()
-    logger.setLevel(logging.INFO)
+    logger.setLevel(log_level)
 
     console_handler = logging.StreamHandler()
     console_handler.setFormatter(log_format)
     logger.handlers = [console_handler]
 
-    if log_file and log_file != '':
+    log_file.parent.mkdir(parents=True, exist_ok=True)
+    if log_file.name and log_file.name != '':
         if rotate:
             file_handler = RotatingFileHandler(
                 log_file, maxBytes=1000000, backupCount=10)
         else:
-            file_handler = logging.FileHandler(log_file)
-        file_handler.setLevel(log_file_level)
+            file_handler = logging.FileHandler(str(log_file))
+        file_handler.setLevel(log_level)
         file_handler.setFormatter(log_format)
         logger.addHandler(file_handler)
 
     return logger
+
+
+def args(parser: ArgumentParser) -> None:
+    parser.add_argument(
+        '-v', '--verbose', action='store_true',
+        dest='verbose',
+        help="Set verbose. In effect, set --log-level to INFO.")
+    parser.set_defaults(verbose=False)
+    parser.add_argument('--log-level', type=LogLevel.__getitem__,
+                        default=LogLevel.INFO,
+                        choices=LogLevel.__members__.values(),
+                        dest='log_level',
+                        help="Log level.")
