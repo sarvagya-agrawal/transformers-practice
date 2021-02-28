929b17d05608ba8d9e7411bdd06db920622a3a53
Mathieu Tuli <tuli.mathieu@gmail.com>
2021-02-28 11:56:28 -0500

    Add project name to setup

--- tod/setup.py
+++ tod/setup.py
@@ -2,32 +2,21 @@ from setuptools import setup
 from glob import glob
 
 setup(
-    name='',
+    name='tod',
     use_scm_version=True,
     packages=[''],
     package_dir={'': 'src'},
     include_package_data=True,
     zip_safe=False,
-    url='https://github.com/mathieutuli/',
-    python_requires='~=3.7',
+    url='https://github.com/MathieuTuli/',
+    python_requires='~=3.8',
     install_requires=[
     ],
     extras_require={
-        'docs': [
-            'sphinx',
-            'sphinx_rtd_theme',
-            'recommonmark',
-        ],
     },
     dependency_links=[
-        # Use SSH-based GitHub auth! HTTPS not acceptable for production use,
-        # due to less flexible authentication.
-        # Yes, you need to add that #egg=… bit, else pip doesn't know which
-        # package in that repo you want to install. A repo may have many or no
-        # packages
     ],
     setup_requires=[
-        'setuptools_scm',  # for git-based versioning
     ],
     # DO NOT do tests_require; just call pytest or python -m pytest.
     license='License :: Other/Proprietary License',
