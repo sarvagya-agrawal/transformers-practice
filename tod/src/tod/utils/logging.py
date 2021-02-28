bd86109ad08deda5d400fe62143a140641fa7a8a
Mathieu Tuli <tuli.mathieu@gmail.com>
2021-02-28 12:07:26 -0500

    Update credit header and add utils logging

---
+++ tod/src/tod/utils/logging.py
@@ -0,0 +1,33 @@
+"""
+@author: OpenNMT-py
+@github: OpenNMT-py
+@link: https://github.com/OpenNMT/OpenNMT-py/blob/master/onmt/utils/logging.py
+"""
+import logging
+from logging.handlers import RotatingFileHandler
+
+logger = logging.getLogger()
+
+
+def init_logger(log_file: str = None,
+                log_file_level=logging.NOTSET,
+                rotate=False):
+    log_format = logging.Formatter("[%(asctime)s %(levelname)s] %(message)s")
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(log_format)
+    logger.handlers = [console_handler]
+
+    if log_file and log_file != '':
+        if rotate:
+            file_handler = RotatingFileHandler(
+                log_file, maxBytes=1000000, backupCount=10)
+        else:
+            file_handler = logging.FileHandler(log_file)
+        file_handler.setLevel(log_file_level)
+        file_handler.setFormatter(log_format)
+        logger.addHandler(file_handler)
+
+    return logger
